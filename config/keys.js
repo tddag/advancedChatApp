3babeffbccd07770acf8c2f30726bc975944b4ec
Hao Bui <iamhaobui@gmail.com>
2019-02-28 23:36:53 -0500

    refactor app

--- config/keys.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    mongoURI: "mongodb://tam:tam123@ds155665.mlab.com:55665/advancedchatapp"
-}
--- server.js
+++ server.js
@@ -1,45 +1,45 @@
-const express = require('express');
-const http = require('http');
-const socketIO = require('socket.io');
-const mongoose = require('mongoose');
-const path = require('path');
+const express = require('express')
+const http = require('http')
+const socketIO = require('socket.io')
+const mongoose = require('mongoose')
+const path = require('path')
 const bodyParser = require('body-parser')
 
 // Import routes
-const history = require('./routes/api/history');
-const room_history = require('./routes/api/room_history');
-const events = require('./routes/api/events');
+const history = require('./back_end/routes/api/history')
+const room_history = require('./back_end/routes/api/room_history')
+const events = require('./back_end/routes/api/events')
 
-const app = express();
+const app = express()
 
-// Middleware 
-app.use(bodyParser.json());
+// Middleware
+app.use(bodyParser.json())
 
 // Create server instance
-const server = http.createServer(app);
+const server = http.createServer(app)
 
 // Create IO using server instance
-const io = socketIO(server);
+const io = socketIO(server)
 
- // Use route
- app.use('/api/history', history);
- app.use('/api/room_history', room_history);
- app.use('/api/events', events);
+// Use route
+app.use('/api/history', history)
+app.use('/api/room_history', room_history)
+app.use('/api/events', events)
 
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
-    // Set static folder
-    app.use(express.static('client/build'))
+  // Set static folder
+  app.use(express.static('client/build'))
 
-    // Set default file for all routes
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'index.html'));
-    })
+  // Set default file for all routes
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'index.html'))
+  })
 }
 
-// Set port 
-const port = process.env.PORT || 4000;
+// Set port
+const port = process.env.PORT || 4000
 
 server.listen(port, () => {
-    console.log(`Server started on port ${port}`);
+  console.log(`Server started on port ${port}`)
 })
