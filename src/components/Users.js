b06d32720b9f2739193b14c7b25eea122e1091d4
Hao Bui <iamhaobui@gmail.com>
2019-03-05 15:45:21 -0500

    updated

--- src/components/Users.js
+++ src/components/Users.js
@@ -41,11 +41,6 @@ class Users extends Component {
         <tr key={index}>
           <th scope="row">{index + 1}</th>
           <td>{user.name}</td>
-          <td>
-            <Link to={`/chat/${user.name}`}>
-              <button>Chat</button>
-            </Link>
-          </td>
         </tr>
       )
     })
@@ -81,7 +76,6 @@ class Users extends Component {
             <tr>
               <th scope="col">#</th>
               <th scope="col">Users</th>
-              <th />
             </tr>
           </thead>
           <tbody>{this.renderUsers()}</tbody>
