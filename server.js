dd36565ae6e15b30e8ded2be10afb9948a42cf1b
franklintrinh367 <franklintrinh367@gmail.com>
2019-03-08 13:07:33 -0500

    commit

--- server.js
+++ server.js
@@ -138,11 +138,11 @@ app.use('/room', room)
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'))
+  app.use(express.static('/build'))
 
   // Set default file for all routes
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
   })
 }
 
